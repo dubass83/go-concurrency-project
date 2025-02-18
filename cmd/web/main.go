0c7af1cdcba8c43845253084a538f1d066f8f1f1
Dubass83 <makssych@gmail.com>
2025-02-18 21:12:07 +0200

    feat: configure connection to the postgres database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,44 @@
 package main
 
-const webPort = "80"
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/dubass83/go-concurrency-project/utils"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
 	// connect to the database
+	conf, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Msg("cannot load configuration")
+	}
+	if conf.Enviroment == "devel" {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+		// log.Debug().Msgf("config values: %+v", conf)
+	}
+
+	// ctx, stop := signal.NotifyContext(context.Background(), interaptSignals...)
+	// defer stop()
+
+	connPool, err := pgxpool.NewWithConfig(context.Background(), poolConfig(conf))
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Msg("cannot validate db connection string")
+	}
+	err = connPool.Ping(context.TODO())
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Msg("cannot ping database")
+	}
 
 	// create sessions
 
@@ -16,4 +51,35 @@ func main() {
 	// set up mail
 
 	// listen for web connections
+}
+
+// PoolConfig create config for db connection pool
+func poolConfig(conf utils.Config) *pgxpool.Config {
+
+	dbConfig, err := pgxpool.ParseConfig(conf.DBSource)
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Msg("failed to create a config")
+	}
+	if conf.DBPoolMaxConns != 0 {
+		dbConfig.MaxConns = conf.DBPoolMaxConns
+	}
+	if conf.DBPoolMinConns != 0 {
+		dbConfig.MinConns = conf.DBPoolMinConns
+	}
+	if conf.DBPoolMaxConnLifetime != time.Second*0 {
+		dbConfig.MaxConnLifetime = conf.DBPoolMaxConnLifetime
+	}
+	if conf.DBPoolMaxConnIdleTime != time.Second*0 {
+		dbConfig.MaxConnIdleTime = conf.DBPoolMaxConnIdleTime
+	}
+	if conf.DBPoolHealthCheckPeriod != time.Second*0 {
+		dbConfig.HealthCheckPeriod = conf.DBPoolHealthCheckPeriod
+	}
+	if conf.DBPoolConnectTimeout != time.Second*0 {
+		dbConfig.ConnConfig.ConnectTimeout = conf.DBPoolConnectTimeout
+	}
+
+	return dbConfig
 }
---
+++ utils/config.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// Config store all configuration of the application
+// the values read by viper from file or enviroment variables
+type Config struct {
+	Enviroment              string        `mapstructure:"ENVIROMENT"`
+	DBSource                string        `mapstructure:"DB_SOURCE"`
+	DBPoolMaxConns          int32         `mapstructure:"DB_POOL_MAX_CONNS"`
+	DBPoolMinConns          int32         `mapstructure:"DB_POOL_MIN_CONNS"`
+	DBPoolMaxConnLifetime   time.Duration `mapstructure:"DB_POOL_MAX_CONN_LIFETIME"`
+	DBPoolMaxConnIdleTime   time.Duration `mapstructure:"DB_POOL_MAX_CONN_IDLE_TIME"`
+	DBPoolHealthCheckPeriod time.Duration `mapstructure:"DB_POOL_HEALTH_CHECK_PERIOD"`
+	DBPoolConnectTimeout    time.Duration `mapstructure:"DB_POOL_CONNECT_TIMEOUT"`
+	MigrationURL            string        `mapstructure:"MIGRATION_URL"`
+	WebPort                 string        `mapstructure:"WEB_PORT"`
+}
+
+// LoadConfig
+func LoadConfig(configPath string) (config Config, err error) {
+	v := viper.New()
+	v.SetConfigName("conf")
+	v.SetConfigType("env")
+	v.AddConfigPath(configPath)
+	err = v.ReadInConfig()
+	if err != nil {
+		return
+	}
+	v.AutomaticEnv()
+	err = v.Unmarshal(&config)
+	return
+}
