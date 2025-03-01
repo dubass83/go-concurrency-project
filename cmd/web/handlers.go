93331e873fb586820e9e0b11e8ca8ea03389798a
Dubass83 <makssych@gmail.com>
2025-03-01 20:38:58 +0200

    chore: add more handlers and routes to the app

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,6 +7,30 @@ import (
 )
 
 func (app *Server) HomePage(w http.ResponseWriter, r *http.Request) {
-	log.Info().Msg("start rendering the home page")
+	log.Debug().Msg("start rendering the home page")
 	app.render(w, r, "home.page.gohtml", nil)
+}
+
+func (app *Server) LoginPage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "login.page.gohtml", nil)
+}
+
+func (app *Server) PostLoginPage(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *Server) Logout(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *Server) RegisterPage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "register.page.gohtml", nil)
+}
+
+func (app *Server) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *Server) ActivateAccount(w http.ResponseWriter, r *http.Request) {
+
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,4 +2,11 @@ package main
 
 func (app *Server) MountHandlers() {
 	app.Router.Get("/", app.HomePage)
+
+	app.Router.Get("/login", app.LoginPage)
+	app.Router.Post("/login", app.PostLoginPage)
+	app.Router.Get("/logout", app.Logout)
+	app.Router.Get("/register", app.RegisterPage)
+	app.Router.Post("/register", app.PostRegisterPage)
+	app.Router.Get("/activate-account", app.ActivateAccount)
 }
