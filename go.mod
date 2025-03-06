35289c784b2ef45cfb07665035b59a4221656955
Dubass83 <makssych@gmail.com>
2025-03-06 21:13:12 +0200

    feat: add function to reset user password by user_id

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-chi/chi/v5 v5.2.1
 	github.com/golang-migrate/migrate/v4 v4.18.2
 	github.com/gomodule/redigo v1.9.2
+	github.com/jackc/pgtype v1.14.0
 	github.com/jackc/pgx/v5 v5.7.2
 	github.com/rs/zerolog v1.33.0
 	github.com/spf13/viper v1.19.0
@@ -19,6 +20,7 @@ require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
--- utils/password.go
+++ utils/password.go
@@ -1,8 +1,12 @@
 package utils
 
 import (
+	"context"
 	"fmt"
+	"time"
 
+	data "github.com/dubass83/go-concurrency-project/data/sqlc"
+	"github.com/jackc/pgx/v5/pgtype"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -21,20 +25,26 @@ func CheckPassword(password, hash string) error {
 }
 
 // ResetPassword is the method we will use to change a user's password.
-// func (u *User) ResetPassword(password string) error {
-// 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
-// 	defer cancel()
+func ResetPassword(password string, user_id int32, store data.Store) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
 
-// 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
-// 	if err != nil {
-// 		return err
-// 	}
+	hashedPassword, err := HashPassword(password)
+	if err != nil {
+		return err
+	}
 
-// 	stmt := `update users set password = $1 where id = $2`
-// 	_, err = db.ExecContext(ctx, stmt, hashedPassword, u.ID)
-// 	if err != nil {
-// 		return err
-// 	}
+	// Update user password
+	_, err = store.UpdateUser(ctx, data.UpdateUserParams{
+		Password: pgtype.Text{
+			String: hashedPassword,
+			Valid:  true,
+		},
+		ID: user_id,
+	})
+	if err != nil {
+		return err
+	}
 
-// 	return nil
-// }
+	return nil
+}
